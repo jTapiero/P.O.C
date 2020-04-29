2f033401583f64e334cec0bb49131f2136d2c7b8
elie daniel tapiero <elie.daniel.tapiero@gmail.com>
2020-04-29 14:35:17 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+// angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// service
+
+// pipe
+
+// page
+import { MainComponent } from './page/main/main.component';
+
+// componnent
+import { FormComponent } from './componnent/form/form.component';
+import { HeaderComponent } from './componnent/header/header.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
